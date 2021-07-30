9e682a6c159884fa3253260b5ba1465340b83bb5
TutorFx <gabriel_serejo@live.com>
2021-07-29 21:50:28 -0300

    Revert "Debug"
    This reverts commit ef67b48094629f5b479e1095a59ac2ff087291a0.

--- layouts/default.vue
+++ layouts/default.vue
@@ -130,6 +130,13 @@ export default {
       }
   },
   mounted() {
+    this.anim = lottie.loadAnimation({
+      container: document.getElementById("hero"), // the dom element that will contain the animation
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      path: "/json/1.json", // the path to the animation json
+    });
   },
 };
 </script>
