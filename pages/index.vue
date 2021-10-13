7506f5183f6584edefea86c420445b3baebabe81
TutorFx <gabriel_serejo@live.com>
2021-10-13 05:30:25 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,25 @@
 </template>
 <script>
 export default {
-  mounted(){},
-  watch:{
-    $route (to, from){
-      if (this.$router.currentRoute.query.scroll != undefined){
-      console.log(this.$router.currentRoute.query.scroll)
-      this.$vuetify.goTo('#' + this.$router.currentRoute.query.scroll)
-    }
-    }
-} 
-}
+  mounted() {},
+  watch: {
+    $route(to, from) {
+      if (this.$router.currentRoute.query.scroll != undefined) {
+        console.log(this.$router.currentRoute.query.scroll);
+        this.$vuetify.goTo("#" + this.$router.currentRoute.query.scroll);
+      }
+    },
+  },
+    head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Veja agora o site pessoal do UX/Motion Designer Gabriel Serejo. Precisa de um design? Vamos trabalhar juntos!'
+      }
+    ],
+  }
+};
 </script>
