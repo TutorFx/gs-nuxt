0790202a6e075b99385bf1a9a50bb3dba049dc47
TutorFx <gabriel_serejo@live.com>
2021-12-07 00:13:17 -0300

    Teste

---
+++ content/portfolio/Agencia Trade.md
@@ -0,0 +1,13 @@
+---
+title: Website Agencia Trade
+description: Identidade Visual desenvolvida para a marca Virtual Labor
+thumbnail: '/virtual labor/assets/thumbnail.png'
+---
+<br>
+
+<v-container>
+
+# Website Agencia Trade
+### Showcase do website feito para a Agência Trade
+
+</v-container>
---
+++ pages/portfolio/_single.vue
@@ -0,0 +1,22 @@
+<template>
+  <nuxt-content :document="articles" />
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, redirect }) {
+    try {
+      const articles = await $content("portfolio", params.single).fetch();
+
+      return {
+        articles,
+      };
+    } catch (error) {
+      return redirect(301, "/404");
+    }
+  },
+};
+</script>
+
+<style>
+</style>
