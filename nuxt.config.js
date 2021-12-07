f9cc82bc154be68d9dd014a70047d7efe6e5e217
TutorFx <gabriel_serejo@live.com>
2021-12-07 12:06:20 -0300

    Debug

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,8 @@ import colors from "vuetify/es5/util/colors";
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: "server",
+  ssr: false,
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
